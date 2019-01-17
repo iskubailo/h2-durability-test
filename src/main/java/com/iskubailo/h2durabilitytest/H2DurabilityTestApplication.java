1ac84863d08fc8424da94fec1a1d9d84fedf2b3a
Igor Skubailo <igor.skubailo@ampard.com>
2019-01-17 14:39:08 +0200

    Initial commit (project generated on start.spring.io)

---
+++ src/main/java/com/iskubailo/h2durabilitytest/H2DurabilityTestApplication.java
@@ -0,0 +1,14 @@
+package com.iskubailo.h2durabilitytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2DurabilityTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2DurabilityTestApplication.class, args);
+	}
+
+}
+
