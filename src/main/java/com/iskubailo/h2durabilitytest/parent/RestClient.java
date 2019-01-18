74266334a77dca59fb010589528885db70b981b4
Igor Skubailo <igor.skubailo@ampard.com>
2019-01-18 09:34:04 +0200

    Use --server.port in RestClient

--- src/main/java/com/iskubailo/h2durabilitytest/parent/RestClient.java
+++ src/main/java/com/iskubailo/h2durabilitytest/parent/RestClient.java
@@ -1,5 +1,6 @@
 package com.iskubailo.h2durabilitytest.parent;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -9,29 +10,35 @@ import com.iskubailo.h2durabilitytest.child.DataEntity;
 @Component
 public class RestClient {
   private final RestTemplate restTemplate;
+  private final String serverPort;
 
-  public RestClient(RestTemplate restTemplate) {
+  public RestClient(RestTemplate restTemplate, @Value("${server.port}") String serverPort) {
     this.restTemplate = restTemplate;
+    this.serverPort = serverPort;
   }
   
   public String helth() {
-    return restTemplate.getForObject("http://localhost:8080/", String.class);
+    return restTemplate.getForObject(getBaseUrl() + "/", String.class);
   }
-  
+
   public DataDto select() {
-    return restTemplate.getForObject("http://localhost:8080/select", DataDto.class);
+    return restTemplate.getForObject(getBaseUrl() + "/select", DataDto.class);
   }
   
   public DataEntity insert() {
-    return restTemplate.getForObject("http://localhost:8080/insert", DataEntity.class);
+    return restTemplate.getForObject(getBaseUrl() + "/insert", DataEntity.class);
   }
   
   public String exit() {
-    return restTemplate.getForObject("http://localhost:8080/exit", String.class);
+    return restTemplate.getForObject(getBaseUrl() + "/exit", String.class);
   }
   
   public String halt() {
-    return restTemplate.getForObject("http://localhost:8080/halt", String.class);
+    return restTemplate.getForObject(getBaseUrl() + "/halt", String.class);
+  }
+  
+  private String getBaseUrl() {
+    return "http://localhost:" + serverPort;
   }
   
 }
